191981aa47a165ff16721e327617d1a1e2048d09
KyraKieskamp <kyra.kieskamp@student.uva.nl>
2015-11-18 14:37:12 +0100

    visualization

---
+++ pygame_visualize_draft.py
@@ -0,0 +1,13 @@
+# Problem Set 6:
+# Visualization code for simulated robots.
+#
+# See the problem set for instructions on how to use this code.
+
+import pygame
+
+(width, height) = (300, 200)
+screen = pygame.display.set_mode((width, height))
+pygame.display.flip
+
+while True:
+    pass
