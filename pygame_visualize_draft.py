77bdeaf3894778e7ee5bb6e2dc7671186b53eaca
KyraKieskamp <kyra.kieskamp@student.uva.nl>
2015-11-18 15:53:18 +0100

    6x6 blue visual board

--- pygame_visualize_draft.py
+++ pygame_visualize_draft.py
@@ -5,9 +5,25 @@
 
 import pygame
 
-(width, height) = (300, 200)
-screen = pygame.display.set_mode((width, height))
-pygame.display.flip
+background_colour = (255, 255, 255)
+(width, height) = (700, 600)
 
-while True:
-    pass
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Rushhour, board 1")
+screen.fill(background_colour)
+
+
+pygame.draw.rect(screen, (0, 0, 255), (20, 20, (width - 40), (height - 40)), 2)
+board_length = 6
+for i in range (1, board_length):
+    xposition = ((width-40)/(board_length)) * i
+    yposition = ((height-40)/(board_length)) * i
+    pygame.draw.line(screen, (0, 0, 255), (20 + xposition, 20), (20 + xposition, height -20))
+    pygame.draw.line(screen, (0, 0, 255), (20, 20 + yposition), (width - 20, 20 + yposition))
+pygame.display.flip()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
