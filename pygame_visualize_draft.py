8d86a3ae4882622a7eca899819335c928505be66
KyraKieskamp <kyra.kieskamp@student.uva.nl>
2015-11-18 16:20:36 +0100

    update pygame

--- pygame_visualize_draft.py
+++ pygame_visualize_draft.py
@@ -12,14 +12,20 @@ screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Rushhour, board 1")
 screen.fill(background_colour)
 
-
+# board frameword
 pygame.draw.rect(screen, (0, 0, 255), (20, 20, (width - 40), (height - 40)), 2)
+
+# board lines
 board_length = 6
 for i in range (1, board_length):
     xposition = ((width-40)/(board_length)) * i
     yposition = ((height-40)/(board_length)) * i
     pygame.draw.line(screen, (0, 0, 255), (20 + xposition, 20), (20 + xposition, height -20))
     pygame.draw.line(screen, (0, 0, 255), (20, 20 + yposition), (width - 20, 20 + yposition))
+
+# 1 car
+pygame.draw.rect(screen, (0, 255, 255), (20, 20, (width - 40), (height - 40)), 2)
+
 pygame.display.flip()
 
 running = True
